a191a53722b29a5feff52aa43481ca030bef8578
nikitos <burylov.2003@mail.ru>
2023-06-16 12:36:42 +0500

    Initial commit

---
+++ PharmacyProject.Domain/Response/BaseResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using PharmacyProject.Domain.Enum;
+
+namespace PharmacyProject.DAL.Response
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        public BaseResponse()
+        {
+        }
+
+        public BaseResponse(string description, StatusCode statusCode, T data)
+        {
+            Description = description;
+            StatusCode = statusCode;
+            Data = data;
+        }
+
+        public string Description { get; set; }
+        public StatusCode StatusCode { get; set; }
+        public T Data { get; set; }
+        
+    }
+
+    public interface IBaseResponse<T>
+    {
+        T Data { get; set; }
+    }
+}
+
